07dc501f8e1fef984455e3b4acbfc1fb8d45eadf
pamelaterragno <pamelaterragno@gmail.com>
2022-08-20 20:32:28 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 
-const BUTTON = document.getElementById("buttomSubmit");
+// const BUTTON = document.getElementById("buttomSubmit");
 
-BUTTON.addEventListener("submit", function(){
-    window.location = "main.html"
-});
+// BUTTON.addEventListener("submit", function(){
+//     window.location = "main.html"
+// });
 
 //this file is named index for an exception, this should be called login

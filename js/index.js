cbdc21af6d234df4a56c9222c34269e88192d7a2
pamelaterragno <pamelaterragno@gmail.com>
2022-08-27 21:12:45 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,11 +4,13 @@ var mail_user = document.getElementById("email-input");
 var pass_user = document.getElementById("password-input");
 
  //save value inputs in local storage
- localStorage.setItem("mail", mail_user.value);
- localStorage.setItem("password", pass_user.value);
+
 
 BUTTON.addEventListener("submit", function(){
+    localStorage.setItem("mail", mail_user.value);
+    localStorage.setItem("password", pass_user.value);
     window.location = "main.html"
+    
 });
 
 //this file is named index for an exception, this should be called login

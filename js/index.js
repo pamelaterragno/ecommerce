63156904af4ae9b570b024e2360bcc93b892b6c6
pamelaterragno <pamelaterragno@gmail.com>
2022-08-30 11:17:26 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,12 +4,20 @@ var mail_user = document.getElementById("email-input");
 var pass_user = document.getElementById("password-input");
 
 BUTTON.addEventListener("click", function() {
-   //save value inputs in local storage
-localStorage.setItem("mail", mail_user.value) 
-localStorage.setItem("password", pass_user.value);
-
-      //redirect to main
-      window.location = "main.html";
+      if (inputRequired) {
+//save value inputs in local storage
+            localStorage.setItem("mail", mail_user.value) 
+            localStorage.setItem("password", pass_user.value);
+//redirect to main
+            window.location = "main.html"; 
+      } else {
+            throw Error
+      }
 });
 
 //this file is named index for an exception, this should be called login
+
+
+function inputRequired() {
+    mail_user.value.length > 0 && pass_user.value.length > 0;
+ } 

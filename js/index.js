9e89f77efb7d0613b47a84f789f2e06d2bcbb94c
pamelaterragno <pamelaterragno@gmail.com>
2022-08-30 09:05:08 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,14 +3,11 @@ const BUTTON = document.getElementById("buttomSubmit");
 var mail_user = document.getElementById("email-input");
 var pass_user = document.getElementById("password-input");
 
-BUTTON.addEventListener("submit", (evt)=> {
+BUTTON.addEventListener("click", function() {
    //save value inputs in local storage
- if (mail_user.value && pass_user.value) {
- localStorage.setItem("mail", mail_user.value) &&
- localStorage.setItem("password", pass_user.value);}
- else {
-  localStorage.removeItem("mail") && localStorage.removeItem("password");
- }
+localStorage.setItem("mail", mail_user.value) 
+localStorage.setItem("password", pass_user.value);
+
       //redirect to main
       window.location = "main.html";
 });

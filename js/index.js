d7333cbe9549496607827d5f431424292ced48fd
pamelaterragno <pamelaterragno@gmail.com>
2022-08-31 18:58:46 -0300

    css yjs

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
 
 const BUTTON = document.getElementById("buttomSubmit");
-var mail_user = document.getElementById("email-input");
-var pass_user = document.getElementById("password-input");
+var mail_user = document.getElementById("email-input").value;
+var pass_user = document.getElementById("password-input").value;
 
 BUTTON.addEventListener("click", function() {
       if (inputRequired) {
 //save value inputs in local storage
-            localStorage.setItem("mail", mail_user.value) 
-            localStorage.setItem("password", pass_user.value);
+            localStorage.setItem("mail", mail_user) 
+            localStorage.setItem("password", pass_user);
 //redirect to main
             window.location = "main.html"; 
       } else {
             //SHOW ERROR
       }
 });
+
+localStorage.getItem("mail")
 
 //this file is named index for an exception, this should be called login
 

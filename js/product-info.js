677920cb4ac04662b9a8ea3e1b669b337007e65b
pamelaterragno <pamelaterragno@gmail.com>
2022-09-12 23:02:32 -0300

    prod

--- js/product-info.js
+++ js/product-info.js
@@ -6,18 +6,45 @@ const PRODUCTS_COMMENTS = PRODUCT_INFO_COMMENTS_URL + localStorage.getItem("prod
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_INFO).then(function(resultObj){
         if (resultObj.status === "ok"){
-            currentProductsArray = resultObj.data.products
-            showProductsList()
+            infoProductsArray = resultObj.data
+            showArrayProd()
 
         }
     });
 
     getJSONData(PRODUCTS_COMMENTS).then(function(resultObj){
         if (resultObj.status === "ok"){
-            currentProductsArray = resultObj.data.products
-            showProductsList()
+            infoProductsArray = resultObj.data
+            showArrayComments()
         }
 
     });
 
 });
+
+function showArrayProd(){
+
+    let htmlContentToAppend = "";
+    
+            htmlContentToAppend += `
+            <div onclick="setProdID(${infoProductsArray.id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${infoProductsArray.image}" alt="${infoProductsArray.description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${infoProductsArray.name}</h4>
+                            <small class="text-muted">${infoProductsArray.soldCount} artículos</small>
+                        </div>
+                        <p class="mb-1">${infoProductsArray.description}</p>
+                        <p class="mb-1">${infoProductsArray.currency} ${infoProductsArray.cost}</p>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+
+        document.getElementById("showProduct").innerHTML = htmlContentToAppend;
+    }
+}

851b3eccd5a7589239334dcf18cd9cb3f6883fe8
pamelaterragno <pamelaterragno@gmail.com>
2022-09-12 23:36:34 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -37,38 +37,34 @@ function showArrayProd(){
                         <p class="mb-1">${infoProductsArray.description}</p>
                         <p class="mb-1">${infoProductsArray.currency} ${infoProductsArray.cost}</p>
                     </div>
+                    
+                  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="${infoProductsArray.images[0]}" class="d-block w-100" alt="first image">
+    </div>
+    <div class="carousel-item">
+      <img src="${infoProductsArray.images[1]}" class="d-block w-100" alt="scn image">
+    </div>
+    <div class="carousel-item">
+      <img src="${infoProductsArray.images[2]}" class="d-block w-100" alt="third image">
+    </div>
+    <div class="carousel-item">
+      <img src="${infoProductsArray.images[3]}" class="d-block w-100" alt="fourth image">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
 
-                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img class="d-block w-100" src="${infoProductsArray.images[0]}" alt="First image">
-                      </div>
-                      <div class="carousel-item">
-                        <img class="d-block w-100" src="${infoProductsArray.images[1]}" alt="Second slide">
-                      </div>
-                      <div class="carousel-item">
-                        <img class="d-block w-100" src="${infoProductsArray.images[2]}" alt="Third slide">
-                      </div>
-                      <div class="carousel-item">
-                        <img class="d-block w-100" src="${infoProductsArray.images[3]}" alt="Third slide">
-                      </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Next</span>
-                    </a>
-                  </div>
+
                 </div>
                 </div>
             `

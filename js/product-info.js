19b4e72b15948188a61af6c02b32044e715471db
pamelaterragno <pamelaterragno@gmail.com>
2022-09-12 23:32:26 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -37,24 +37,37 @@ function showArrayProd(){
                         <p class="mb-1">${infoProductsArray.description}</p>
                         <p class="mb-1">${infoProductsArray.currency} ${infoProductsArray.cost}</p>
                     </div>
-                    
-                    <div class="row">
-                    
-                    <div class="col">
-                    <img src="${infoProductsArray.images[0]}" class="img-thumbnail">
-                    </div>
 
-                    <div class="col">
-                    <img src="${infoProductsArray.images[1]}" class="img-thumbnail">
-                    </div>
 
-                    <div class="col">
-                    <img src="${infoProductsArray.images[2]}" class="img-thumbnail">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img class="d-block w-100" src="${infoProductsArray.images[0]}" alt="First image">
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="${infoProductsArray.images[1]}" alt="Second slide">
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="${infoProductsArray.images[2]}" alt="Third slide">
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="${infoProductsArray.images[3]}" alt="Third slide">
+                      </div>
                     </div>
-
-                    <div class="col">
-                    <img src="${infoProductsArray.images[3]}" class="img-thumbnail">
-                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
                 </div>
                 </div>
             `

4f2efde95a8620b07986e0947a13b0444d0c04c8
pamelaterragno <pamelaterragno@gmail.com>
2022-09-13 00:46:32 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -32,7 +32,7 @@ function showArrayProd(){
                    
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${infoProductsArray.name}</h4>
+                            <h1 class="text-center">${infoProductsArray.name}</h1>
                         
                         </div>
                         <p class="mb-1">${infoProductsArray.description}</p>

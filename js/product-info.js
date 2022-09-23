ceedbb4070dd5bf199efc608360bc0c60d3ea3a7
pamelaterragno <pamelaterragno@gmail.com>
2022-09-22 22:28:43 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -91,7 +91,7 @@ function showRelated(){
   htmlContentToAppend+=`
   
   
-  <div class="card" style="width: 18rem; display: flex;">
+  <div class="card" style="width: 18rem;">
   <img src="${a.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="${a.name}">${a.name}</h5>

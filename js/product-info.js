7a28527a4777a1adbf509b42c81f5ea98e7b4cd4
pamelaterragno <pamelaterragno@gmail.com>
2022-09-22 22:26:33 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -96,7 +96,7 @@ function showRelated(){
   <div class="card-body">
     <h5 class="${a.name}">${a.name}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="PRODUCTS_INFO" class="btn btn-primary">Comprar</a>
+    <a href="product-info.html" class="btn btn-primary">Comprar</a>
   </div>
 </div>
 }

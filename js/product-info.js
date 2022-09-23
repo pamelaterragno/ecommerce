b3c33b680e17010dff1e0532e5eeb6018aa1e845
pamelaterragno <pamelaterragno@gmail.com>
2022-09-22 22:42:39 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -96,7 +96,7 @@ function showRelated(){
   <div class="card-body">
     <h5 class="name">${a.name}</h5>
     
-    <a href="product-info.html" class="btn btn-primary">Comprar</a>
+    <a href="product-info.html" class="btn btn-primary">Ver más</a>
   </div>
 </div>
 

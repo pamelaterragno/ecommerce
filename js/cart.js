457baa1671c13364ece9f7735c58a946669d8e95
pamelaterragno <pamelaterragno@gmail.com>
2022-10-10 12:09:41 -0300

    cart

--- js/cart.js
+++ js/cart.js
@@ -4,9 +4,30 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(INFO_USER).then(function(resultObj){
         if (resultObj.status === "ok"){
             infoUserExapmle = resultObj.data
-          
+          showCart()
         }
     })});
 
 
-    console.log(infoUserExapmle)
+   function showCart(){
+let htmlContentToAppend ="";
+
+for(let i = 0; i < infoUserExapmle.articles.length; i++){
+    let a = infoUserExapmle.articles[i];
+
+htmlContentToAppend+= `
+
+<div class="card" style="width: 20%;">
+  <img src="${a.image}" class="card-img-top" alt="${a.name}">
+  <div class="card-body">
+    <h5 class="name">${a.name}</h5>
+    
+    <a href="product-info.html" onclick="setProdID(${a.id})" class="btn btn-primary">Ver más</a>
+  </div>
+</div>
+
+
+`
+
+document.getElementById("showCart").innerHTML = htmlContentToAppend
+   }}

bc90a433e916fbfffe5e0bf025783cb0f9a87636
pamelaterragno <pamelaterragno@gmail.com>
2022-10-14 15:51:28 -0300

    a

--- js/cart.js
+++ js/cart.js
@@ -1,38 +1,64 @@
 const INFO_USER = CART_INFO_URL + "25801" + EXT_TYPE;
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(INFO_USER).then(function(resultObj){
-        if (resultObj.status === "ok"){
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(INFO_USER).then(function (resultObj) {
+        if (resultObj.status === "ok") {
             infoUserExapmle = resultObj.data
-          showCart()
+            showCart()
         }
-    })});
+    })
+});
 
 
-   function showCart(){
-let htmlContentToAppend ="";
+function showCart() {
+    let htmlContentToAppend = "";
 
-for(let i = 0; i < infoUserExapmle.articles.length; i++){
-    let a = infoUserExapmle.articles[i];
+    for (let i = 0; i < infoUserExapmle.articles.length; i++) {
+        let a = infoUserExapmle.articles[i];
 
-htmlContentToAppend+= `
+        htmlContentToAppend += `
 
+<div class="container">
 
-<div class="row border-top border-bottom">
-<div class="row main align-items-center">
-    <div class="col-2"><img class="img-fluid" src="${a.image}"></div>
-    <div class="col">
-        <div class="row text-muted">${a.name}</div>
-        <div class="row">${a.unitCost} ${a.currency} </div>
+            <div class="d-flex flex-row justify-content-between align-items-center pt-lg-4 pt-2 pb-3 border-bottom mobile" id="heading">
+            <div></div>
+                <div class="px-lg-5 mr-lg-5" id="product">Producto</div>
+                <div class="px-lg-5 ml-lg-5" id="price">Precio</div>
+                <div class="px-lg-5 ml-lg-1" id="quantity">Cantidad</div>
+                <div class="px-lg-5 ml-lg-3" id="subtotal">Subtotal</div>
+            </div>
+            <div class="d-flex flex-row justify-content-between align-items-center pt-lg-4 pt-2 pb-3 border-bottom mobile">
+                <div class="d-flex flex-row align-items-center">
+                    <div><img src="${a.image}" width="150"  alt="" id="image" class="me-5"></div>
+                    <div class="d-flex flex-column pl-md-3 pl-1">
+                    
+                        <div><h3>${a.name}</h3></div>
+                       
+                        
+                    </div>                    
+                </div>
+                <div class="pl-md-0 pl-1"><b>${a.currency} ${a.unitCost}</b></div>
+                <div class="pl-md-0 pl-2">
+                    <span class="fa fa text-secondary"></span><input size="1" id="cantidad" class="px-md-1 px-1"  value="${a.count}"></span><span class="fa fa text-secondary"></span>
+                </div>
+                <div class="pl-md-0 pl-1"><b>${a.currency} ${a.unitCost}</b></div>
+                <div class="close">&times;</div>
+            </div>
+           
+
+        </div>
     </div>
-    <div class="col">
-        <a href="#">-</a><a href="#" class="border">1</a><a href="#">+</a>
-    </div>
-    <div class="col">&euro; 44.00 <span class="close">&#10005;</span></div>
 </div>
 </div>
-
 `
-}
-document.getElementById("showCarrito").innerHTML = htmlContentToAppend
-   };
+        console.log(a.unitCost, a.count)
+        console.log(a.unitCost * a.count)
+
+
+
+    }
+
+    document.getElementById("showCarrito").innerHTML = htmlContentToAppend
+};
+
+

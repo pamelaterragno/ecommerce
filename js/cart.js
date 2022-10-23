9becc1594cd911d6a86cb053c2992149f0a895f3
pamelaterragno <pamelaterragno@gmail.com>
2022-10-23 11:00:34 -0300

    update cart

--- js/cart.js
+++ js/cart.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
             showCart();
             totalPrice();
         }
-    }) 
+    })
 });
 
 // show cart esta pensado para array de prodcutos, en este caso solamente tenemos un ejemplo en particular
@@ -24,46 +24,31 @@ function showCart() {
 
 
 
-        htmlContentToAppend += `
+        htmlContentToAppend += 
+        `
+        <div class="d-flex flex-row justify-content-between align-items-center pt-lg-4 pt-2 pb-3 border-bottom mobile">
+        <div class="d-flex flex-row align-items-center">
 
-<div class="container">
+<div><img src="${a.image}" width="150"  alt="" id="image" class="me-5"></div>
+<div class="d-flex flex-column pl-md-3 pl-1">
 
-            <div class="d-flex flex-row justify-content-between align-items-center pt-lg-4 pt-2 pb-3 border-bottom mobile" id="heading">
-            <div></div>
-                <div class="px-lg-5 mr-lg-5" id="product">Producto</div>
-                <div class="px-lg-5 ml-lg-5" id="price">Precio</div>
-                <div class="px-lg-5 ml-lg-1" id="quantity">Cantidad</div>
-                <div class="px-lg-5 ml-lg-3" id="subtotal">Subtotal</div>
-            </div>
-            <div class="d-flex flex-row justify-content-between align-items-center pt-lg-4 pt-2 pb-3 border-bottom mobile">
-                <div class="d-flex flex-row align-items-center">
-                    <div><img src="${a.image}" width="150"  alt="" id="image" class="me-5"></div>
-                    <div class="d-flex flex-column pl-md-3 pl-1">
-                    
-                        <div><h3>${a.name}</h3></div>
-                       
-                        
-                    </div>                    
-                </div>
-                <div class="pl-md-0 pl-1 d-flex justify-content-evenly" >${a.currency} <div id="cost" >${a.unitCost}</div></div>
-                <div class="pl-md-0 pl-2">
-                    <input size="1" id="cantidad" class="px-md-1 px-1 form-control" oninput="totalPrice()" value="${a.count}"  type="number" name="costinput"  min="0" >
-                </div>
-                
-                <div class="pl-md-0 pl-1 d-flex justify-content-evenly">${a.currency}<div id="showSubtotal"></div></div>
-                <div class="close">&times;</div>
-            </div>
-           
-
-        </div>
-    </div>
+    <div><h3>${a.name}</h3></div>
+   
+    
+</div>                    
 </div>
+<div class="pl-md-0 pl-1 d-flex justify-content-evenly" >${a.currency} <div id="cost" >${a.unitCost}</div></div>
+<div class="pl-md-0 pl-2">
+<input size="1" id="cantidad" class="px-md-1 px-1 form-control" oninput="totalPrice()" value="${a.count}"  type="number" name="costinput"  min="0" >
 </div>
+
+<div class="pl-md-0 pl-1 d-flex justify-content-evenly">${a.currency}<div id="showSubtotal"></div></div>
+<div class="close">&times;</div>
+
+
 `
-
-
-
     }
+    
 
     document.getElementById("showCarrito").innerHTML = htmlContentToAppend
 };
@@ -72,9 +57,9 @@ function showCart() {
 // function calcSubtotal() {
 //     let cantidadArt = parseInt(document.getElementById("cantidad").value);
 //     let costo = parseInt(document.getElementById("cost").innerHTML);
- 
+
 //         return cantidadArt * costo;
-    
+
 // }
 
 
@@ -84,10 +69,12 @@ function totalPrice() {
     const costo = document.getElementById("cost").innerHTML;
     const carritosubtotal = document.getElementById("showSubtotal");
 
-        console.log(costo)
-        console.log(cantidadUni)
+    console.log(costo)
+    console.log(cantidadUni)
 
-return carritosubtotal.innerHTML = cantidadUni*costo;
+    return carritosubtotal.innerHTML = cantidadUni * costo;
 
-} ;
+};
+
+
 

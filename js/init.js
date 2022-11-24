08831019aaa55a5e5e27d4426fcb22cb69f5649a
pamelaterragno <pamelaterragno@gmail.com>
2022-11-24 15:27:08 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,11 +1,14 @@
-const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
-const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
-const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
-const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
-const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
-const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
-const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
+const LOCAL_HOST = "http://localhost:3000/"
+
+const CATEGORIES_URL = LOCAL_HOST + "ecommerce/emercado-api-main/cats";
+const PUBLISH_PRODUCT_URL = LOCAL_HOST + "ecommerce/emercado-api-main/sell";
+const PRODUCTS_URL = LOCAL_HOST + "ecommerce/emercado-api-main/cats_products/";
+const PRODUCT_INFO_URL = LOCAL_HOST + "ecommerce/emercado-api-main/products/";
+const PRODUCT_INFO_COMMENTS_URL = LOCAL_HOST + "ecommerce/emercado-api-main/products_comments/";
+const CART_INFO_URL = LOCAL_HOST + "ecommerce/emercado-api-main/user_cart";
+const CART_BUY_URL = LOCAL_HOST + "ecommerce/emercado-api-main/cart";
 const EXT_TYPE = ".json";
+
 const LOG_OUT = document.getElementById("logOut");
 
 let showSpinner = function(){
